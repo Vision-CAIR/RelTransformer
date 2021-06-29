97451ae5beed9acedc862f5d64b5c97035a16a9b
junchen14 <jun.chen@kaust.edu.sa>
2021-06-29 15:04:36 +0300

    upload

---
+++ lib/modeling/transformer.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+from torch.nn.parameter import Parameter
+import math
+
+def gelu(x):
+    return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        """Construct a layernorm module in the TF style (epsilon inside the square root).
+        """
+        super(LayerNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.bias = nn.Parameter(torch.zeros(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+        return self.weight * x + self.bias
+
+class Conv1D_(nn.Module):
+    def __init__(self, nf, nx):
+        super(Conv1D_, self).__init__()
+        self.nf = nf
+        w = torch.empty(nx, nf)
+        nn.init.normal_(w, std=0.02)
+        self.weight = Parameter(w)
+        self.bias = Parameter(torch.zeros(nf))
+
+    def forward(self, x):
+        size_out = x.size()[:-1] + (self.nf,)
+        test = x.contiguous().view(-1, x.size(-1))
+        x = torch.addmm(self.bias, x.contiguous().view(-1, x.size(-1)), self.weight)
+
+        x = x.view(*size_out)
+        return x
+
